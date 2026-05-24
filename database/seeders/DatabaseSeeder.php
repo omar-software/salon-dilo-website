d94c4b3c9336c6e243160193e8deceed772f070f
Omar Mohamad-Ali <oa4ev@hotmail.com>
2026-05-24 16:32:35 +0200

    Add admin database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
-     * Seed the application's database.
+     * Seeder für die Datenbank starten.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // AdminSeeder ausführen
+        $this->call(AdminSeeder::class);
     }
 }
